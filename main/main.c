8cbd77602b368b6c1f07ad60eb4da52ce02035d3
Sebastian Giraldo <sebastian.giraldo@titoma.com>
2022-10-17 12:43:00 -0500

    feat: initial project was created

---
+++ main/main.c
@@ -0,0 +1,42 @@
+/**
+ * Application entry point.
+ */
+
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+
+static const char TAG[] = "[main]";
+
+#define MAIN_DEBUG_ENABLE
+#ifdef MAIN_DEBUG_ENABLE
+	#define MAIN_DEBUG(...) ESP_LOGI(TAG,LOG_COLOR(LOG_COLOR_CYAN) __VA_ARGS__)
+#else
+	#define MAIN_DEBUG(...)
+#endif
+
+
+void nvs_flash_setup(void)
+{
+	 // Initialize NVS
+	esp_err_t ret = nvs_flash_init();
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+	{
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(ret);
+}
+
+void app_main(void)
+{
+
+	MAIN_DEBUG("HEAP MEMORY: %d\n",esp_get_free_heap_size());
+
+	nvs_flash_setup(); 
+
+}
+
